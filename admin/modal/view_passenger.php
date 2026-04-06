ff74068302caea13bbf6d8c308be5e76386f10a6
Jhed Sarmiento <chryslrosrmnt@gmail.com>
2026-04-06 17:14:33 +0800

    Add files via upload

---
+++ admin/modal/view_passenger.php
@@ -0,0 +1,29 @@
+<div class="modal fade" id="modal-view-pass">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Confirmation</h4>
+			</div>
+			<div class="modal-body">
+			<center>
+				<strong>Book By: </strong><span id="book-by"></span> <br />
+				<strong>Schedule Date: </strong><span id="date"></span> <br />
+				<strong>Email: </strong><span id="contact"></span> <br />
+				<strong>Address: </strong><span id="address"></span><br />
+			</center>
+				<br />
+				<div id="passenger-list">
+					
+				</div>
+			</div>
+			<div class="modal-footer">
+				
+
+				<button type="button" onclick="addTransaction();" class="btn btn-success accept-pay">Accept
+				<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
